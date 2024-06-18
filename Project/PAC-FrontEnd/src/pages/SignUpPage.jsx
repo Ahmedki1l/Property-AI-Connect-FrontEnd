fd9ae79d59b330dbfa44e4d5257a47e7a1345a21
Ahmedki1l <ahmedzzz456@gmail.com>
2024-06-18 19:25:56 +0300

    Create SignUpPage component in ahmed-branch

---
+++ Project/PAC-FrontEnd/src/pages/SignUpPage.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+
+import '../css/SignUpPage.css';
+
+const SignUpPage = () => {
+  const handleGoogleSignUp = () => {
+    // Handle Google Sign Up
+  };
+
+  return (
+    <div className='signUpLayout'>
+      <NavBar />
+      <div className='signUpContent'>
+        <h1>Sign Up</h1>
+        <form>
+          <div className='formGroup'>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id='email' name='email' required />
+          </div>
+          <div className='formGroup'>
+            <label htmlFor='password'>Password:</label>
+            <input type='password' id='password' name='password' required />
+          </div>
+          <button type='submit'>Sign Up</button>
+        </form>
+        <button className='googleSignUp' onClick={handleGoogleSignUp}>
+          Sign Up with Google
+        </button>
+        <p>
+          Already have an account? <Link to='/login'>Login</Link>
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default SignUpPage;
